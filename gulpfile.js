84ba2549a5a4f594484ae7743df8005a97032aec
Lee908105576 <908105576@qq.com>
2016-07-04 15:06:45 +0800

    修改watch,去掉版本号，添加默认任务

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var gulp=require('gulp'),
     tinypng=require('gulp-tinypng'),//使用tinypng插件压缩png和jpg图片
     notify=require('gulp-notify'),//编译错误时，显示通知，保证watch继续
     autoprefixer=require('gulp-autoprefixer'),//添加前缀css
-    orig=require('gulp-rev-orig'),//自动在html中添加版本控制
     reload=require('gulp-livereload'),//保存刷新页面
     concat=require('gulp-concat'),//合并js
     uglify=require('gulp-uglify'),//压缩js代码
@@ -13,21 +12,16 @@ var gulp=require('gulp'),
 
 
 gulp.task('html',function(){
-    return gulp.src('./src/*.html')
-        .pipe(orig({//版本号为watch完成时的时间
-          revType: 'date',
-          dateFormat: 'yymmddHHmm'
-        }))
-        .pipe(gulp.dest('./dist'))
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'))
         .pipe(reload())
 })
 
 gulp.task('js',function(){
-    return gulp.src('./src/js/*.js')
+    return gulp.src('src/js/*.js')
         .pipe(concat('all.js'))//合并后的名字
         .pipe(uglify())//压缩js
-        .pipe(gulp.dest('./src/js'))
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest('dist/js'))
 })
 
 //编译less
@@ -42,7 +36,7 @@ gulp.task('less',function(){
         })(err);
         this.emit('end');
     }
-    return gulp.src('./src/css/*.less')
+    return gulp.src('src/css/*.less')
         //通过plumber将错误信息提交给notify显示出来
         .pipe(plumber({errorHandler: onError}))
         .pipe(less())
@@ -52,8 +46,7 @@ gulp.task('less',function(){
         }))
         .pipe(concat('all.css'))
         .pipe(minicss())    //压缩css
-        .pipe(gulp.dest('./src/css'))
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('dist/css'))
         //成功后notify通知
         .pipe(notify({ // Add gulpif here
             title: 'Gulp',
@@ -66,25 +59,28 @@ gulp.task('less',function(){
 
 //使用tinypng对图片进行高压缩
 gulp.task('img',function(){
-    return gulp.src('./src/img/*.{png,jpg,gif,icon}')
+    return gulp.src('src/img/*.{png,jpg,gif,icon}')
         .pipe(tinypng('53LWY9FK4uVlB3rFJ7lx2X6jh4NhT62L'))//tinypng插件的密钥
-        .pipe(gulp.dest('./dist/img'))
+        .pipe(gulp.dest('dist/img'))
         .pipe(reload())
 })
 
 //监听事件
 gulp.task('watch',function(){
     reload.listen()
-    gulp.watch('./src/*.html',function(){
+    gulp.watch('src/*.html',function(){
         gulp.run('html')
     })
-    gulp.watch('./src/js/*.js',function(){
+    gulp.watch('src/js/*.js',function(){
         gulp.run('js')
     })
-    gulp.watch('./src/css/*.less',function(){
+    gulp.watch('src/css/*.less',function(){
         gulp.run('less')
     })
-    gulp.watch('./src/img/*.{png,jpg,gif,icon}',function(){
+    gulp.watch('src/img/*.{png,jpg,gif,icon}',function(){
         gulp.run('img')
     })
 })
+
+//默认任务--命令行执行gulp
+gulp.task('default',['html','js','less','watch'])
